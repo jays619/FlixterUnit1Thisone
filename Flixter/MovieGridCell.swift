99f0b86595ed63bbb75322adc5451ff9a79aaf65
said h <66662384+jays619@users.noreply.github.com>
2020-10-25 21:34:23 -0700

    new files

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Said J Hofiani on 10/25/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
